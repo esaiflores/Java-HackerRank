08dace76e61657977148235e266e201247b6ea6a
Esai Flores <flores.esaijonathan@gmail.com>
2020-08-10 20:41:12 -0500

    making array list

---
+++ hacker-rank/src/arrayList.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class arrayList {
+
+    public static void main(String[] args) {
+
+        List<String> stringList = new ArrayList<>();
+
+        stringList.add("bob");
+        stringList.add("Linda");
+        stringList.add("Flankton");
+    }
+}
