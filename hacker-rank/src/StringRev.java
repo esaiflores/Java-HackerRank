dbd2727c2a7ae6562702a764f4c46d621d3b52e6
Esai Flores <flores.esaijonathan@gmail.com>
2020-08-17 18:24:51 -0500

    coding challenge

---
+++ hacker-rank/src/StringRev.java
@@ -0,0 +1,12 @@
+public class StringRev {
+
+    public static String rev(String s){
+        return new StringBuilder(s).reverse().toString();
+    }
+    public static void main(String[] args){
+        String s= "Welcome to Edureka"; // Note that string is immutable in Java
+        s= rev(s);
+        System.out.println("Result after reversing a string is : "+s);
+    }
+
+}
